54f8ec787a581ae49afd6bca475ac43c32172bd9
Aditya <adityarabbyoka3@gmail.com>
2025-05-04 18:32:59 +0700

    update deskripsi

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,8 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "AniMangaListo",
-  description: "Website List Anime Terlengkap",
+  description:
+    "AniMangaListo is a comprehensive online platform tailored for anime enthusiasts of all levels — from newcomers exploring the world of Japanese animation to seasoned fans seeking their next favorite series. Our curated database features a wide variety of anime across multiple genres, including action, adventure, romance, fantasy, and slice of life, each accompanied by detailed descriptions, episode counts, user ratings, and personalized recommendations. Designed with a clean, intuitive interface and powerful search functionality, AniMangaListo enables users to effortlessly discover titles that align with their preferences and current mood. Enhance your viewing experience with our watchlist feature , which allows you to bookmark anime for future viewing — making it easier than ever to organize and track your anime journey. Whether you're looking for the latest seasonal releases or timeless classics, Anime List serves as your ultimate guide to navigating the vast and captivating world of anime.",
 };
 
 export default function RootLayout({ children }) {
