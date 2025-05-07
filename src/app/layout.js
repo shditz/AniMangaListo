1c4590f6df4aa15f9227ff2d22d4a1b39acfe5d5
Aditya <adityarabbyoka3@gmail.com>
2025-05-07 09:59:53 +0700

    sda

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/src/app/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/icon192x192.png"
+        />
+        <link rel="apple-touch-icon" href="/public/appleicon.png" />
+        <link rel="manifest" href="/public/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
