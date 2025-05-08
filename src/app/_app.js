646951494f7780e38abc3773b56fad8b425577db
Aditya <adityarabbyoka3@gmail.com>
2025-05-08 09:48:28 +0700

    ss

--- src/app/_app.js
+++ src/app/_app.js
@@ -2,31 +2,16 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((reg) => console.log("Service Worker Registered", reg))
+        .catch((err) => console.log("SW registration failed: ", err));
+    }
+  }, []);
+
   return <Component {...pageProps} />;
-}
-
-// Tambahkan kode ini di luar fungsi MyApp
-if (typeof window !== "undefined") {
-  window.addEventListener("load", () => {
-    const installBtn = document.getElementById("install-btn");
-
-    let deferredPrompt;
-
-    window.addEventListener("beforeinstallprompt", (e) => {
-      e.preventDefault();
-      deferredPrompt = e;
-      installBtn.style.display = "block";
-    });
-
-    installBtn.addEventListener("click", async () => {
-      if (!deferredPrompt) return;
-      deferredPrompt.prompt();
-      const { outcome } = await deferredPrompt.userChoice;
-      installBtn.style.display = "none";
-      deferredPrompt = null;
-      console.log("User choice:", outcome);
-    });
-  });
 }
 
 export default MyApp;
